3f56c85cda1e34e631e61a2e812437fc48a2cd8c
Mihael <miwaque@gmail.com>
2024-05-12 15:40:01 +0200

    Dodao sam inicijalne datoteke za mvn spring aplikaciju.

---
+++ MLib/src/test/java/com/infobip/pmf/course/MLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infobip.pmf.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
